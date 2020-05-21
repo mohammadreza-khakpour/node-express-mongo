96353761233659f0ef3ef99fab9b27807c6bb6e7
f14-tomcat <rezakhakpoorboraz@gmail.com>
2020-05-20 21:55:16 -0700

    2nd

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 const fs = require(`fs`);
 const textIn = fs.readFileSync(`./txt/input.txt`,`utf-8`);
 console.log(textIn);
+// how to write to files:
+const textOut = `About Avocado:\n${textIn}`;
+fs.writeFileSync(`./txt/output.txt`,textOut);
+console.log(`writing to the output.txt is done`);
