d2313605cd4572e603d6a6e878055e3c6728aa7c
f14-tomcat <rezakhakpoorboraz@gmail.com>
2020-05-23 00:02:30 -0700

    http module started

--- index.js
+++ index.js
@@ -8,18 +8,27 @@
 // console.log(`writing to the output.txt is done`);
 
 // reading file, asyncronous style (non-blocking style)
-const fs2 = require(`fs`);
-fs2.readFile(`./txt/start.txt`,`utf-8`,(err, data)=>{
-    console.log(`data:\n${data}\n`);
-    fs2.readFile(`./txt/${data}.txt`,`utf-8`,(err,data2)=>{
-        console.log(`data2:\n${data2}\n`);
-        fs2.readFile(`./txt/append.txt`,`utf-8`,(err, data3)=>{
-            console.log(`data3:\n${data3}\n`);
-            fs2.writeFile(`./txt/final.txt`,`${data2}++++\n+++++${data3}\n`,(vv)=>{
-                console.log(`Done ! ... Written`);
-            });
+// const fs2 = require(`fs`);
+// fs2.readFile(`./txt/start.txt`,`utf-8`,(err, data)=>{
+//     console.log(`data:\n${data}\n`);
+//     fs2.readFile(`./txt/${data}.txt`,`utf-8`,(err,data2)=>{
+//         console.log(`data2:\n${data2}\n`);
+//         fs2.readFile(`./txt/append.txt`,`utf-8`,(err, data3)=>{
+//             console.log(`data3:\n${data3}\n`);
+//             fs2.writeFile(`./txt/final.txt`,`${data2}++++\n+++++${data3}\n`,(vv)=>{
+//                 console.log(`Done ! ... Written`);
+//             });
             
-        })
-    })
+//         })
+//     })
+// });
+// console.log(`is working on files....`);
+
+///////////////// Http module /////////////////////////
+const http = require(`http`);
+const server = http.createServer((req,res)=>{
+    res.end(`---- http server created ----`);
 });
-console.log(`is working on files....`);
+server.listen(8000,`127.0.0.1`,()=>{
+   console.log(`---- Listening ----`);
+});
